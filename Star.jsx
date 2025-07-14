7f44216113475944c22b62afb0b1abdb857ae613
takeda <takeda@mpocket.jp>
2025-07-14 18:32:51 +0900

    Updates

--- Star.jsx
+++
@@ -1,8 +0,0 @@
-import { FaStar } from "react-icons/fa";
-import React from "react";
-
-export const Star = ({selected = false, onSelect = f => f }) => {
-  return(
-    <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
-  )
-};
